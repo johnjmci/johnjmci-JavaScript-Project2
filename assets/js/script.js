b3699317c5dd37ea99fe78d75db9476bd5c0e451
johnjmci <mcivorjohnj@gmail.com>
2022-02-06 14:22:10 +0000

    Add initial JavaScript and functions

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const BeginGame = document.getElementById('welcome')
+
+BeginGame.addEventListener('click', newGame)
+
+function newGame() {
+    console.log('STARTED')
+    BeginGame.classList.add('hidden')
+}
+
+function showQuestion() {
+
+}
+
+function chooseAswer() {
+
+}
+function displayResults() {
+
+}
