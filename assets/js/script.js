631345d17e7236695fd547949fa52a120248ba2c
johnjmci <mcivorjohnj@gmail.com>
2022-02-06 18:13:20 +0000

    Add button listener for answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,59 @@
-const BeginGame = document.getElementById('startButton')
-const questionElement = document.getElementById('question')
-const answerElement = document.getElementById('answer-area-buttons')
-
-let shuffledQuestions, currentQuestionIndex
-
+const BeginGame = document.getElementById('welcome')
 BeginGame.addEventListener('click', newGame)
 
 function newGame() {
-    console.log('STARTED')
     BeginGame.classList.add('hidden')
-    showNextQuestion()
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionindex = 0
-    
 }
 
-function showNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionindex])
 
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-
-}
-
-function chooseAswer() {
-
-}
-function displayResults() {
-
-}
-
-const questions = [
+let question = 
     {
-        question: 'How many brothers does Jim Halpert have?',
-        answers: [
-            {text: '2', correct: true},
-            {text: '3', correct: false},
-            {text: '4', correct: false}
-        ]
+        title: 'How many brothers does Jim Halpert have?',
+        alternatives: ['2', '3', '4'],
+        correctAnswer: 0
+    };
+
+function showQuestion(q) {
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent = q.title;
+
+    let alts = document.querySelectorAll('.alternative');
+    alts.forEach(function(element, index){
+
+    element.textContent = q.alternatives[index];
+
+    element.addEventListener('click', function(){
+        //! Check if the answer chosen is the corect one //!
+        if(q.correctAnswer == index) {
+            console.log('Correct Answer!');
+        }
+        else {}
+        console.log('Wrong Answer!');
+
+    });
+    
+
+
+});
+}
+showQuestion(question);
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    console.log('CLICKED!')
+});
+
+var score = 0;
+
+
+for(var i=0; 1 < questions.length; i++) {
+    var response = window.prompt(questions[i].prompt)
+    if (response == question[i].answer){
+        score++;
+        alert('correct');
+    } else {
+        alert'(WRONG!');
     }
-]
+    }
+alert('You got' + score + '/' + questions.length;)
