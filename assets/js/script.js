2ba00f5313f43ef44eb69d1e0e5046d7114b914c
johnjmci <mcivorjohnj@gmail.com>
2022-02-06 20:03:43 +0000

    Clear out and recreate JavaScript structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,26 @@
 const BeginGame = document.getElementById('welcome')
 BeginGame.addEventListener('click', newGame)
-
+ 
 function newGame() {
-    BeginGame.classList.add('hidden')
+   BeginGame.classList.add('hidden')
+}
+ 
+function runGame(){
+
 }
 
+function checkAnswer() {
 
-let question = 
-    {
-        title: 'How many brothers does Jim Halpert have?',
-        alternatives: ['2', '3', '4'],
-        correctAnswer: 0
-    };
+}
 
-function showQuestion(q) {
-    let titleDiv = document.getElementById('title');
-    titleDiv.textContent = q.title;
+function addWrongSore() {
 
-    let alts = document.querySelectorAll('.alternative');
-    alts.forEach(function(element, index){
+}
 
-    element.textContent = q.alternatives[index];
+function addRightSore() {
 
-    element.addEventListener('click', function(){
-        //! Check if the answer chosen is the corect one //!
-        if(q.correctAnswer == index) {
-            console.log('Correct Answer!');
-        }
-        else {}
-        console.log('Wrong Answer!');
+}
 
-    });
+function displayQuestion() {
     
-
-
-});
 }
-showQuestion(question);
-
-let btn = document.getElementById('btn');
-
-btn.addEventListener('click', function(){
-    console.log('CLICKED!')
-});
-
-var score = 0;
-
-
-for(var i=0; 1 < questions.length; i++) {
-    var response = window.prompt(questions[i].prompt)
-    if (response == question[i].answer){
-        score++;
-        alert('correct');
-    } else {
-        alert'(WRONG!');
-    }
-    }
-alert('You got' + score + '/' + questions.length;)
