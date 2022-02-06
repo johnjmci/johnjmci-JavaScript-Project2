803cdd8cda16fe75ba343d861d0488e0ffef51f6
johnjmci <mcivorjohnj@gmail.com>
2022-02-06 15:40:09 +0000

    Initial JavaScript 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
-const BeginGame = document.getElementById('welcome')
+const BeginGame = document.getElementById('startButton')
+const questionElement = document.getElementById('question')
+const answerElement = document.getElementById('answer-area-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 BeginGame.addEventListener('click', newGame)
 
 function newGame() {
     console.log('STARTED')
     BeginGame.classList.add('hidden')
+    showNextQuestion()
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionindex = 0
+    
 }
 
-function showQuestion() {
+function showNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionindex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
 
 }
 
@@ -17,3 +31,14 @@ function chooseAswer() {
 function displayResults() {
 
 }
+
+const questions = [
+    {
+        question: 'How many brothers does Jim Halpert have?',
+        answers: [
+            {text: '2', correct: true},
+            {text: '3', correct: false},
+            {text: '4', correct: false}
+        ]
+    }
+]
