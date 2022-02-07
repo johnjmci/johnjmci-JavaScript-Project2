9724969de64573332e558b59dceced5685ce4b20
johnjmci <mcivorjohnj@gmail.com>
2022-02-07 18:31:52 +0000

    Add responses based on whether answer is correct or incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,10 +76,19 @@ choices.forEach(choice => {
       acceptingAnswers = false;
       const selectedChoice = e.target;
       const selectedAnswer = selectedChoice.dataset["number"];
+
+      const classToApply = 
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+      selectedChoice.parentElement.classList.add(classToApply);
       
-      getNewQuestion();
-   })
-} )
+      setTimeout(() => {
+         selectedChoice.parentElement.classList.remove(classToApply);
+         getNewQuestion();
+      }, 1000);
+           
+   });
+});
 
 
 startGame();
