290d574f4ea9076e1aa51b85dfe4172495f8ff4c
johnjmci <mcivorjohnj@gmail.com>
2022-02-07 16:21:38 +0000

    Update JavaScript to contain questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,53 @@
-const BeginGame = document.getElementById('welcome')
-BeginGame.addEventListener('click', newGame)
+// instruction to display and then hide welcome text when game starts 
+const BeginGame = document.getElementById("welcome")
+BeginGame.addEventListener("click", newGame)
  
 function newGame() {
-   BeginGame.classList.add('hidden')
-}
- 
-function runGame(){
-
+   BeginGame.classList.add("hidden")
 }
 
-function checkAnswer() {
+// create question and answer dynamic
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
-}
+let currentQuestion = {},
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
-function addWrongSore() {
+let questions = [
+   { 
+   question: "What is the names of Jim and Pam Halpert's son?”,
+   choice1: "Charles”,
+   choice2: "Phillip”,
+   choice3: "Edward”,
+   answer: 2 
+   }, 
 
-}
+]
 
-function addRightSore() {
 
-}
+// constants necessary for game to function
 
-function displayQuestion() {
+const correct_bonus = 10;
+const max_questions = 3;
+
+startGame = () => {
+   questionCounter = 0;
+   score = 0;
+   availableQuestions = [...questions];
+   console.log(availableQuestions);
+   getNewQuestion();
+};
+
+getNewQuestion() = () => {
+   questionCounter++;
+   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+   currentQuestion = availableQuestions[questionIndex];
+   question.innerText = currentQuestion.question;
     
-}
+};
+
+
+startGame();
