01c0875dc7191f3ada7712181272de92dad3e7d8
johnjmci <mcivorjohnj@gmail.com>
2022-02-08 20:14:29 +0000

    update order of text on quiz page

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,12 @@
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
+
+
+/*   feedback message to player based on final score 
+100 = Regional Manager
+70 to 90 = Assistant to the Regional Manager
+40 to 60 = Sales Representative
+10 to 30 = Temp
+0 = Intern
+*/
